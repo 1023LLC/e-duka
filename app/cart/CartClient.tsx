61b39fba891d527a6266814f81ed165df0e9f71f
julius <nyerereoffice@gmail.com>
2024-07-05 19:47:53 +0300

    Display Total Cart Quantity

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -6,9 +6,10 @@ import { MdArrowBack } from "react-icons/md"
 import Heading from "../components/Heading";
 import Button from "../components/Button";
 import ItemContent from "./ItemContent";
+import { formatPrice } from "@/utils/formatPrice";
 
 const CartClient = () => {
-    const {cartProducts, handleClearCart} = useCart()
+    const {cartProducts, handleClearCart, cartTotalAmount} = useCart()
     if(!cartProducts || cartProducts.length === 0){
         return(
             <div className="flex flex-col items-center">
@@ -45,7 +46,7 @@ const CartClient = () => {
             <div className="text-sm flex flex-col gap-1 items-start">
                 <div className="flex justify-between w-full text-base font-semibold">
                     <span>Subtotal</span>
-                    <span>KES 1,000</span>
+                    <span>{formatPrice(cartTotalAmount)}</span>
                 </div>
                 <p className="text-slate-500 mb-2">Taxes and shipping calculated at checkout</p>
 
