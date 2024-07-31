af7c75398fd836a617cb038724bf262a918d8247
julius <nyerereoffice@gmail.com>
2024-07-31 10:35:15 +0300

    Manage Orders

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Container from "@/app/components/Container";
+import ManageOrdersClient from "./ManageOrdersClient";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import getOrders from "@/actions/getOrders";
 
-const ManageOrders = () => {
+
+
+const ManageOrders = async () => {
+
+const orders = await getOrders()
+const currentUser = await getCurrentUser()
+
+if(!currentUser || currentUser.role !== 'ADMIN'){
+  return <NullData title="Oops! Access denied"/>
+}
+
   return (
-    <div>ManageOrders</div>
+    <div className="pt-8">
+      <Container>
+        <ManageOrdersClient orders = {orders}/>
+      </Container>
+    </div>
   )
 }
 
-export default ManageOrders
+export default ManageOrders;
