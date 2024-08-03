11d2bc2d8e79e7b372e84938237c58ec2adc0a01
julius <nyerereoffice@gmail.com>
2024-08-03 09:29:44 +0300

    Fetch Products From DB and Display

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'firebasestorage.googleapis.com', 
+      'lh3.googleusercontent.com',
+      'm.media-amazon.com'
+    ],
+  },
+};
+
+module.exports = nextConfig;
